9cee60dbd2c08885e19f1f1fc005be4f883e1aa1
chichobs <chichobs206@gmail.com>
2025-11-21 22:30:18 -0400

    Actualizar Plantilla.cpp

---
+++ Acciones, Funcionea y Pase por Parámetros/Plantilla.cpp
@@ -0,0 +1,13 @@
+//Plantilla
+
+TipoDeRetorno NombreFuncion(TipoParametro1, TipoParametro2, ...);
+// Prototipo: la función devuelve un 'int' y acepta dos 'int' como parámetros.
+int sumar(int a, int b);
+
+TipoDeRetorno NombreFuncion(TipoParametro1 nombreParametro1, TipoParametro2 nombreParametro2) {
+    // Cuerpo de la función
+    
+    // El operador 'return' devuelve el valor del tipo especificado,
+    // o simplemente 'return;' si el TipoDeRetorno es 'void'.
+    return expresion_o_variable; 
+}
