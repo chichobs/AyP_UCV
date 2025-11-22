e2ac33e72c3972a5b4913272f35c7a7cac0ba5cd
chichobs <chichobs206@gmail.com>
2025-11-21 22:35:31 -0400

    Actualizar Plantilla.cpp

---
+++ Arreglos y Matrices/Plantilla.cpp
@@ -0,0 +1,16 @@
+//Plantilla 
+
+TipoDeDato nombreArray[tamaño];
+int edades[5]; // Array para guardar 5 enteros.
+double salarios[100]; // Array para guardar 100 números decimales.
+TipoDeDato nombreArray[] = {valor1, valor2, valor3, ...};
+
+TipoDeDato nombreMatriz[filas][columnas];
+int tablero[3][3] = {
+    {1, 2, 3}, // Fila 0
+    {4, 5, 6}, // Fila 1
+    {7, 8, 9}  // Fila 2
+};
+
+// Acceder al elemento en la Fila 1, Columna 2 (que es el valor 6):
+cout << tablero[1][2]; // Output: 6
